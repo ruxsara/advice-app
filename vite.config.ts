36ef241ba1a4ffbd2e4e5f22adfb67768e2c8b04
ruxsara <87611734+ruxsara@users.noreply.github.com>
2023-12-25 19:56:07 +0400

    Feat: create Base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {BASE} from './src/utility/constants.js'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base:`/${BASE}/`,  
   plugins: [react()],
 })
